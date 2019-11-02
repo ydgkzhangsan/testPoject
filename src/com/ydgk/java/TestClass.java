b114a9d64d42678bdf4ca71e121a3944e10d277c
tom <tom@qq.com>
2019-11-02 11:59:35 +0800

    这是我的第一次提交的IDEA代码

---
+++ src/com/ydgk/java/TestClass.java
@@ -0,0 +1,12 @@
+package com.ydgk.java;
+
+/**
+ * @author kfstart
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("测试IDEA提交代码到远程库");
+    }
+
+}
